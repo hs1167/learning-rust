6f3c3ac79c1f6c56a6a2895b356fde88e4c260e7
hs1167 <254142453+hs1167@users.noreply.github.com>
2026-01-11 00:50:03 +0100

    fix: save changes to main.rs properly

--- basics/variables/src/main.rs
+++ basics/variables/src/main.rs
@@ -1,5 +1,5 @@
 fn main(){
-    let mut x = 5;
+    let mut x = 5; //test to ensure mutability of variable x
     println!("the value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
