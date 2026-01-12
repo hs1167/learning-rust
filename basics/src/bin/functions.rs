a34da7ee7697c7f2546d7743d4d4136785e0f353
hs1167 <254142453+hs1167@users.noreply.github.com>
2026-01-12 16:23:35 +0100

    wip(basics): experiment with functions

--- basics/src/bin/functions.rs
+++ basics/src/bin/functions.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Exercice Functions");
+    println!("Hello, world!");
+
+    another_function(5);
+}
+
+fn another_function(x: i32){
+    println!("The value of x is: {x}");
 }
