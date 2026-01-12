05fa8f5e43d5526260ed9ceb0aadea43f6ab6c57
hs1167 <254142453+hs1167@users.noreply.github.com>
2026-01-12 15:13:26 +0100

    feat: add week 1 struct exercise

---
+++ basics/target/debug/deps/week1_structs-5e5065cdf8da4c1d.d
@@ -0,0 +1,5 @@
+/home/hs1167/dev/learning-rust-zk/basics/target/debug/deps/week1_structs-5e5065cdf8da4c1d.d: src/bin/week1_structs.rs
+
+/home/hs1167/dev/learning-rust-zk/basics/target/debug/deps/week1_structs-5e5065cdf8da4c1d: src/bin/week1_structs.rs
+
+src/bin/week1_structs.rs:
---
+++ basics/target/debug/week1_structs.d
@@ -0,0 +1,1 @@
+/home/hs1167/dev/learning-rust-zk/basics/target/debug/week1_structs: /home/hs1167/dev/learning-rust-zk/basics/src/bin/week1_structs.rs
