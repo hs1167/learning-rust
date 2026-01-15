7882b3c1d4fa4ade426b544b3784b56834f9b177
hs1167 <254142453+hs1167@users.noreply.github.com>
2026-01-16 00:43:49 +0100

    feat: complete all structs rustlings exercises

--- basics/src/bin/structs.rs
+++ basics/src/bin/structs.rs
@@ -1,14 +1,5 @@
-fn main() {
-    let _user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
+#[derive(Debug)]
+
+fn main(){
+    dbg!("hehe");
 }
-struct User{
-        active: bool,
-        username: String,
-        email: String,
-        sign_in_count: u64,
-    }
