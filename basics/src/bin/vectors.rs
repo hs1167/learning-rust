71508f651c31ee4645e96a002e885cb09fbf86eb
hs1167 <254142453+hs1167@users.noreply.github.com>
2026-01-18 12:00:57 +0100

    feat: trick to use copy trait on element of a vec

--- basics/src/bin/vectors.rs
+++ basics/src/bin/vectors.rs
@@ -1,7 +1,7 @@
 fn main(){
     let mut v = vec![1, 2, 3, 4, 5];
 
-    let first = v[0];
+    let first = &v[0]; // here we just can remove the borrow &, to use the copy trait on i32
 
     v.push(6);
 
