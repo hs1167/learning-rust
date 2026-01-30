d6f0dbb3153570dcd862d67a749757627878f5d1
hs1167 <254142453+hs1167@users.noreply.github.com>
2026-01-30 23:52:26 +0100

    im back

--- basics/src/bin/sandbox.rs
+++ basics/src/bin/sandbox.rs
@@ -1,3 +1,15 @@
-fn main() {
-    
+fn gcd (mut a: u64, mut b: u64) -> u64 {
+    while b != 0 {
+        let mut remainder = a%b;
+        a = b;
+        b = remainder;
+    }
+    a
+}
+
+
+
+fn main () {
+    let c = gcd(17,23);
+    println!("{c}");
 }
